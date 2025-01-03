d450f0fd2e732b579380069c88a3d921eb3e0a6b
Lwazi <lwazigxogxa@gmail.com>
2025-01-03 10:07:57 +0200

    master java document

---
+++ files/test.java
@@ -0,0 +1,118 @@
+ /* Write a program to Sort Numeric Array In Ascending Order*/
+ private static void activity1(){
+
+         }
+
+ /* Write a program to Sort Numeric Array In Descending Order */
+ private static void activity2(){
+
+         }
+
+ /*Write a program to print sum values of an array */
+ private static void activity3(){
+
+         }
+
+ /*Write a program to calculate the average value of array elements */
+ private static void activity4(){
+
+         }
+
+ /*Write a program to store elements in an array and print it*/
+ private static void activity5(){
+
+         }
+
+ /*Write a Java program to test if an array contains a specific value.*/
+ private static void activity6(){
+
+         }
+
+//    Write a Java program to find the index of an array element.
+ private static void activity7(){
+
+         }
+
+//    Write a Java program to remove a specific element from an array.
+ private static void activity8(){
+
+         }
+
+//    Write a Java program to copy an array by iterating the array.
+ private static void activity9(){
+
+         }
+
+//    Write a Java program to insert an element (specific position) into an array.
+ private static void activity10(){
+
+         }
+
+//    Write a Java program to find the maximum and minimum value of an array.
+ private static void activity11(){
+
+         }
+
+//    Write a Java program to reverse an array of integer values.
+ private static void activity12(){
+
+         }
+
+//    Write a Java program to find duplicate values in an array of integer values.
+//    Write a Java program to find duplicate values in an array of string values.
+ private static void activity13(){
+
+         }
+
+//    Write a Java program to find common elements between two arrays (string values).
+//    Write a Java program to find common elements between two integer arrays.
+ private static void activity14(){
+
+         }
+
+//    Write a Java program to convert an array to an ArrayList.
+//    Write a Java program to convert an ArrayList to an array.
+ private static void activity15(){
+
+         }
+
+//    Write a Java program to find all pairs of elements in an array whose sum is equal to a specified number.
+ private static void activity16(){
+
+         }
+
+//    Write a Java program to test two arrays' equality.
+ private static void activity17(){
+
+         }
+
+//    Write a Java program to find the number of even and odd integers in a given array of integers.
+ private static void activity18(){
+
+         }
+
+//    Write a Java program to separate even and odd numbers from a given array of integers. Put all even numbers first, and then odd numbers.
+ private static void activity19(){
+
+         }
+
+//    Write a Java program to rearrange a given array of unique elements such that every second element of the array is greater than its left and right elements.
+ private static void activity20(){
+
+         }
+
+/*    Write a Java program to print the following grid.
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+            - - - - - - - - - -
+      */
+ private static void activity21(){
+
+         }
